4a8c1b214d830ef9a45a03dfe038507450c5dc66
ralphtheninja <ralphtheninja@riseup.net>
2014-12-10 01:03:41 +0100

    First commit

---
+++ 02-servo-wave.js
@@ -0,0 +1,11 @@
+var five = require('johnny-five')
+var board = new five.Board()
+
+board.on('ready', function () {
+  var servo = new five.Servo(9)
+  servo.sweep()
+  this.wait(3000, function () {
+    servo.stop()
+    servo.center()
+  })
+})
